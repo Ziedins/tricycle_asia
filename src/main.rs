e988be022c3e19a42f4834f579f3861709246b9f
Reinis Ziedins <reinis.z@legalbet.com>
2023-03-23 13:21:51 +0700

    greet people system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
 
 fn hello_world() {
     println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("John Goodman".to_string())));
+    commands.spawn((Person, Name("Santa Claus".to_string())));
+    commands.spawn((Person, Name("Robert Deniro".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>)
+{
+   for name in &query {
+        print!("Hello {}", name.0);
+   }
+
 }
