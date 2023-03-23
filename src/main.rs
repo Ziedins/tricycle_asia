365d51ce725e3db770c8bd19d3b78e255005f3d2
Reinis Ziedins <reinis.z@legalbet.com>
2023-03-23 13:57:06 +0700

    Added timer that greets people

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -13,9 +16,6 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("John Goodman".to_string())));
@@ -23,19 +23,22 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Robert Deniro".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>)
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>)
 {
-   for name in &query {
-        print!("Hello {}", name.0);
-   }
 
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}", name.0);
+        }
+    }
 }
 
 pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-        .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_startup_system(add_people)
         .add_system(greet_people);
     }
 }
+
