8076f5fde2300e7533b05a63e2795634049316c8
Reinis Ziedins <reinis.z@legalbet.com>
2023-03-23 14:24:43 +0700

    Added tricycle sprite

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,16 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
         .run();
 }
 
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("John Goodman".to_string())));
-    commands.spawn((Person, Name("Santa Claus".to_string())));
-    commands.spawn((Person, Name("Robert Deniro".to_string())));
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("sprite/racing/tricycle-1.png"),
+        ..default()
+    });
 }
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>)
-{
-
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hello {}", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-        .add_startup_system(add_people)
-        .add_system(greet_people);
-    }
-}
-
